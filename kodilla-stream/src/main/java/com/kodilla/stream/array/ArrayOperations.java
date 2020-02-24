2c23d3fdfa93fc5a064197b4a3c69f1de5ea256a
radoslawwojtyla <radoslaw.wojtyla@gmail.com>
2020-02-24 12:46:04 +0100

    Module 7.5 - dodanie .map do strumienia nr 1 (tranformacja nr indeksu na wartoć z tablicy)

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -7,6 +7,7 @@ import java.util.stream.IntStream;
 public interface ArrayOperations {
     static double getAverage(int[] numbers) {
         IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
                 .forEach(System.out::print);
 
         return IntStream.range(0, numbers.length)
