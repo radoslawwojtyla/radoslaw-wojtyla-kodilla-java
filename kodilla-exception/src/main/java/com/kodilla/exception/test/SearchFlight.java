d582cf88afe57d82ebfac45853038d2a1cccb040
radoslawwojtyla <radoslaw.wojtyla@gmail.com>
2020-03-22 14:28:20 +0100

    Module 8.4: changed findFlight method logic; added main method

--- kodilla-exception/src/main/java/com/kodilla/exception/test/SearchFlight.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/SearchFlight.java
@@ -1,21 +1,11 @@
 package com.kodilla.exception.test;
 
-//        Stwórz klasę zawierającą logikę imitującą wyszukiwanie lotów:
-//        a) metoda findFilght(Flight flight)
-//        b) wewnątrz metody zaimplementuj mapę typu HashMap, której kluczem będzie String przechowujący nazwę lotniska,
-//        a wartością Boolean informujący o tym czy można na dane lotnisko polecieć.
-//        Przykład: Map<String, Boolean>.
-//        c) logika powinna wyszukiwać loty w mapie.
-//        Jeżeli dane lotnisko nie będzie istnieć w mapie, należy rzucić wyjątkiem RouteNotFoundException.
-
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class SearchFlight {
 
-
-    public void findFlight(Flight flight) throws RouteNotFoundException {
+    public static boolean findFlight(Flight flight) throws RouteNotFoundException {
         Map<String, Boolean> arrivalAirport = new HashMap<>();
         arrivalAirport.put("Tokyo", true);
         arrivalAirport.put("Berlin", false);
@@ -23,10 +13,21 @@ public class SearchFlight {
         arrivalAirport.put("Kiev", true);
         arrivalAirport.put("London", false);
 
-        if (arrivalAirport.containsKey(flight.getArrivalAirport()) == true && flight.getDepartureAirport() != null) {
-            System.out.println("The airport exist.");
-        } else {
+        if (arrivalAirport.containsKey(flight.getArrivalAirport()) == false || flight.getDepartureAirport() == null) {
             throw new RouteNotFoundException("The airport doesn't exist");
+        } else {
+            return arrivalAirport.get(flight.getArrivalAirport());
+        }
+    }
+
+    public static void main(String[] args) {
+        Flight flight = new Flight("London", "Kiev");
+        try {
+            findFlight(flight);
+            System.out.println("The airport exists");
+        } catch (RouteNotFoundException e) {
+            System.out.println("The flight is unavailable");
+            System.out.println("Error: " + e);
         }
     }
 }
