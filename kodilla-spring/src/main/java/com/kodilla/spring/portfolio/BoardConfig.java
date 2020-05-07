e8004a16f0d567d64a4581fdf1c5fa3085aeebf4
radoslawwojtyla <radoslaw.wojtyla@gmail.com>
2020-05-07 19:08:50 +0200

    using beans from container

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,27 +1,36 @@
 package com.kodilla.spring.portfolio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BoardConfig {
+    @Autowired
+    @Qualifier("toDoList")
+    TaskList toDoList;
+    @Autowired
+    TaskList inProgressList;
+    @Autowired
+    TaskList doneList;
 
     @Bean
     public Board getBoard() {
-        return new Board(getToDoList(), getInProgressList(), getDoneList());
+        return new Board(toDoList, inProgressList, doneList);
     }
 
-    @Bean
+    @Bean (name = "toDoList")
     public TaskList getToDoList() {
         return new TaskList();
     }
 
-    @Bean
+    @Bean (name = "inProgressList")
     public TaskList getInProgressList() {
         return new TaskList();
     }
 
-    @Bean
+    @Bean (name = "doneList")
     public TaskList getDoneList() {
         return new TaskList();
     }
