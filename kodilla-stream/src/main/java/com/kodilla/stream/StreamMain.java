b5b78333ed67d75300ac8136717008551d889ce0
radoslawwojtyla <radoslaw.wojtyla@gmail.com>
2020-05-15 12:01:51 +0200

    add hibernate

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,27 +1,12 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.book.Book;
-import com.kodilla.stream.book.BookDirectory;
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
-import com.kodilla.stream.iterate.NumbersGenerator;
-import com.kodilla.stream.lambda.ExecuteSaySomething;
-import com.kodilla.stream.lambda.Executor;
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.lambda.Processor;
-import com.kodilla.stream.person.People;
-import com.kodilla.stream.reference.FunctionalCalculator;
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-import jdk.nashorn.internal.objects.NativeString;
 
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
-import static jdk.nashorn.internal.objects.NativeString.toUpperCase;
 
 public class StreamMain {
     public static void main(String[] args) {
