68d0c8309e84300c682f029ccaeeb5c5847cfc67
radoslawwojtyla <radoslaw.wojtyla@gmail.com>
2020-09-16 15:07:00 +0200

    Ex 20.3 Aspect

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/WatcherFacade.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/WatcherFacade.java
@@ -8,8 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
-
 @Aspect
 @Component
 public class WatcherFacade {
@@ -21,18 +19,18 @@ public class WatcherFacade {
         LOGGER.info("Class: " + object.getClass().getName() + ", Args: " + order);
     }
 
-//    @Around("execution(* com.kodilla.patterns2.aop.calculator.Calculator.factorial(..))")
-//    public Object measureTime(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-//        Object result;
-//        try {
-//            long begin = System.currentTimeMillis();
-//            result = proceedingJoinPoint.proceed();
-//            long end = System.currentTimeMillis();
-//            LOGGER.info("Time consumed: " + (end - begin) + " [ms]");
-//        } catch (Throwable throwable) {
-//            LOGGER.error(throwable.getMessage());
-//            throw throwable;
-//        }
-//        return result;
-//    }
+    @Around("execution(* com.kodilla.patterns2.aop.calculator.Calculator.factorial(..))")
+    public Object measureTime(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object result;
+        try {
+            long begin = System.currentTimeMillis();
+            result = proceedingJoinPoint.proceed();
+            long end = System.currentTimeMillis();
+            LOGGER.info("Time consumed: " + (end - begin) + " [ms]");
+        } catch (Throwable throwable) {
+            LOGGER.error(throwable.getMessage());
+            throw throwable;
+        }
+        return result;
+    }
 }
